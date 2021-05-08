85d6572fb45c6dfac074d6733965169f2b3e0ff5
Gabriel Alves Moreira <g.alves.m.2008@gmail.com>
2021-05-08 11:33:25 -0300

    Initial commit

---
+++ LoginMVVM/Classes/ViewController/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  LoginMVVM
+//
+//  Created by Gabriel Alves Moreira on 08/05/21.
+//  Copyright © 2021 Gabriel Alves Moreira. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .white
+    }
+}
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'LoginMVVM' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for LoginMVVM
+  pod "RxSwift"
+  pod "RxCocoa"
+
+end
