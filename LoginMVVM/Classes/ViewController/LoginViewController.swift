7566975f0e6aa21fe72e09f03f21d97e99b515f0
Gabriel Alves Moreira <g.alves.m.2008@gmail.com>
2021-05-08 11:57:32 -0300

    Create login screen

--- LoginMVVM/Classes/ViewController/LoginViewController.swift
+++ LoginMVVM/Classes/ViewController/LoginViewController.swift
@@ -10,7 +10,66 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    private lazy var stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 36
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    private lazy var usernameTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.autocapitalizationType = .none
+        textField.placeholder = "Username"
+        return textField
+    }()
+    
+    private lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.autocapitalizationType = .none
+        textField.placeholder = "Password"
+        return textField
+    }()
+    
+    private lazy var loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("LOGIN", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 6
+        return button
+    }()
+    
     override func viewDidLoad() {
         view.backgroundColor = .white
+        setupView()
+    }
+    
+    private func setupView() {
+        setupHierarchy()
+        setupConstraints()
+    }
+    
+    private func setupHierarchy() {
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(usernameTextField)
+        stackView.addArrangedSubview(passwordTextField)
+        stackView.addArrangedSubview(loginButton)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            usernameTextField.heightAnchor.constraint(equalToConstant: 36),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 36),
+            loginButton.heightAnchor.constraint(equalToConstant: 48)
+        ])
     }
 }
